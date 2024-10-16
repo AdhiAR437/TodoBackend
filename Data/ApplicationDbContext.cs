114e88759e17495199cff7c814b8c06157482083
Likhith Kemminje <likem@eg.dk>
2024-10-16 17:29:41 +0530

    Feat: created Db context and dataModels

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoBackend.Models;
+
+namespace TodoBackend.Data
+{
+	public class ApplicationDbContext:Microsoft.EntityFrameworkCore.DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+		public DbSet<UserModel> Users_tb { get; set; }
+		public DbSet<TasksModel> Tasks_tb { get; set; }
+	}
+}
